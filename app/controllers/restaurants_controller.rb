7820beb56c59e32786c6eb2bfd71a7465493ed3b
Gilbert Huarcaya <gilberthuarcaya@gmail.com>
2022-01-19 17:03:59 -0500

    all done

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -1,5 +1,5 @@
 class RestaurantsController < ApplicationController
-  before_action :set_restaurant, only: %i[ show edit update destroy ]
+  before_action :set_restaurant, only: %i[ show ]
 
   # GET /restaurants or /restaurants.json
   def index
@@ -16,8 +16,6 @@ class RestaurantsController < ApplicationController
   end
 
   # GET /restaurants/1/edit
-  def edit
-  end
 
   # POST /restaurants or /restaurants.json
   def create
@@ -35,36 +33,18 @@ class RestaurantsController < ApplicationController
   end
 
   # PATCH/PUT /restaurants/1 or /restaurants/1.json
-  def update
-    respond_to do |format|
-      if @restaurant.update(restaurant_params)
-        format.html { redirect_to restaurant_url(@restaurant), notice: "Restaurant was successfully updated." }
-        format.json { render :show, status: :ok, location: @restaurant }
-      else
-        format.html { render :edit, status: :unprocessable_entity }
-        format.json { render json: @restaurant.errors, status: :unprocessable_entity }
-      end
-    end
-  end
 
   # DELETE /restaurants/1 or /restaurants/1.json
-  def destroy
-    @restaurant.destroy
-
-    respond_to do |format|
-      format.html { redirect_to restaurants_url, notice: "Restaurant was successfully destroyed." }
-      format.json { head :no_content }
-    end
-  end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_restaurant
-      @restaurant = Restaurant.find(params[:id])
-    end
 
-    # Only allow a list of trusted parameters through.
-    def restaurant_params
-      params.require(:restaurant).permit(:name, :address, :phone_number, :category)
-    end
+  # Use callbacks to share common setup or constraints between actions.
+  def set_restaurant
+    @restaurant = Restaurant.find(params[:id])
+  end
+
+  # Only allow a list of trusted parameters through.
+  def restaurant_params
+    params.require(:restaurant).permit(:name, :address, :phone_number, :category)
+  end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,39 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+attributes = {
+  name: "La Tour d'Argent",
+  address: "15 Quai de la Tournelle, 75005 Paris",
+  phone_number: "86743456",
+  category: "french",
+}
+attributes1 = {
+  name: "Pollito",
+  address: "15 Quai de la Tournelle, 75005 Paris",
+  phone_number: "23423423",
+  category: "french",
+}
+attributes2 = {
+  name: "cevichito",
+  address: "15 Quai de la Tournelle, 75005 Paris",
+  phone_number: "02342431",
+  category: "french",
+}
+attributes3 = {
+  name: "Del mar",
+  address: "15 Quai de la Tournelle, 75005 Paris",
+  phone_number: "02354431",
+  category: "french",
+}
+attributes4 = {
+  name: "Casita",
+  address: "15 Quai de la Tournelle, 75005 Paris",
+  phone_number: "12352433",
+  category: "italian",
+}
+Restaurant.create(attributes)
+Restaurant.create(attributes1)
+Restaurant.create(attributes2)
+Restaurant.create(attributes3)
+Restaurant.create(attributes4)
